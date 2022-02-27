02d0cd46a06071154e08fde5c8423baa5b7e8850
CharReek <charlottereek@outlook.com>
2022-02-27 21:32:56 +0000

    add styling

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const headerSection = document.getElementsById("header-section")
+const leftSection = document.getElementById("left-section")
+const rightSection = document.getElementById("right-section")
