75f8f9213d01d8335614e35d45dc92e46eea8a5e
CharReek <charlottereek@outlook.com>
2022-03-16 20:55:36 +0000

    add jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
-function showHide(){
-    var x = document.getElementById("when-where");
-    if (x.style.display === "none"){
-        x.style.display = "block"; 
-    } else {
-        x.style.display = "none";
-    }
-}
-
-function showHideTwo(){
-    var x = document.getElementById("other-info");
-    if (x.style.display === "none"){
-        x.style.display = "block"; 
-    } else {
-        x.style.display = "none";
-    }
-}
+$(document).ready(function(){
+    $(".left-section").mouseenter(function(){
+    $(this).css("background-color", "red")
+    });
+    $(".left-section").mouseleave(function(){
+    $(this).css("background-color", "")
+    });
+    $(".right-section").mouseenter(function(){
+    $(this).css("background-color", "red")
+    });
+    $(".right-section").mouseleave(function(){
+    $(this).css("background-color", "")
+    });
+});
