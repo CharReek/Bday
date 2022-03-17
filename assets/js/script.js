a1d909a02923e3797260f0b0fd82edb9af612c88
CharReek <charlottereek@outlook.com>
2022-03-17 21:26:16 +0000

    add css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
     $(".left-section").mouseenter(function(){
-    $(this).css("background-color", "red")
+        $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
     });
     $(".left-section").mouseleave(function(){
-    $(this).css("background-color", "")
+        $(this).css("background-color", "")
     });
     $(".right-section").mouseenter(function(){
-    $(this).css("background-color", "red")
+         $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
     });
     $(".right-section").mouseleave(function(){
-    $(this).css("background-color", "")
+        $(this).css("background-color", "")
     });
+    $(".header-section").mouseenter(function(){
+        $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
+    });
+     $(".header-section").mouseleave(function(){
+        $(this).css("background-color", "")
+    });
+    
+
 });
