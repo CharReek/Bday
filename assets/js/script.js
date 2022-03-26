1418c9ea254532ffed86eeac1ece47236ebb02e0
CharReek <charlottereek@outlook.com>
2022-03-26 21:30:51 +0000

    plat about

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,18 @@
-$(document).ready(function(){
-    $(".left-section").mouseenter(function(){
-        $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
-    });
-    $(".left-section").mouseleave(function(){
-        $(this).css("background-color", "")
-    });
-    $(".right-section").mouseenter(function(){
-         $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
-    });
-    $(".right-section").mouseleave(function(){
-        $(this).css("background-color", "")
-    });
-    $(".header-section").mouseenter(function(){
-        $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
-    });
-     $(".header-section").mouseleave(function(){
-        $(this).css("background-color", "")
-    });
-    
-
+$(".left-section").mouseenter(function () {
+    $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
+});
+$(".left-section").mouseleave(function () {
+    $(this).css("background-color", "")
+});
+$(".right-section").mouseenter(function () {
+    $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
+});
+$(".right-section").mouseleave(function () {
+    $(this).css("background-color", "")
+});
+$(".header-section").mouseenter(function () {
+    $(this).css("background-color", "rgba(0, 0, 0, 0.8)")
+});
+$(".header-section").mouseleave(function () {
+    $(this).css("background-color", "")
 });
